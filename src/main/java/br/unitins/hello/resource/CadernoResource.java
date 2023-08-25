58cfd1f207848a1591a3af4a021c583347cf4ba3
muniznicole <nicoleamuniz@gmail.com>
2023-08-25 20:58:50 -0300

    ADD PUT AND DELETE

--- src/main/java/br/unitins/hello/resource/CadernoResource.java
+++ src/main/java/br/unitins/hello/resource/CadernoResource.java
@@ -3,20 +3,85 @@ package br.unitins.hello.resource;
 import java.util.List;
 
 import br.unitins.hello.model.Caderno;
-
+import br.unitins.hello.repository.CadernoRepository;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/cadernos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CadernoResource {
 
-    //Lista todos os cadernos
+    @Inject
+    CadernoRepository repository;
+
+    // Insere um novo caderno
+    @POST
+    @Transactional
+    public Caderno insert(Caderno caderno){
+        Caderno novoCaderno = new Caderno();
+        novoCaderno.setTitulo(caderno.getTitulo());
+        novoCaderno.setColecao(caderno.getColecao());
+        novoCaderno.setTamanho(caderno.getTamanho());
+
+        repository.persist(novoCaderno);
+
+        return novoCaderno;
+    }
+
+    // Lista todos os cadernos
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Caderno> todosCadernos() {
-        return Caderno.listAll();
+    public List<Caderno> findAll(){
+        return repository.listAll();
+    }
+
+    // Consulta por id
+    @GET
+    @Path("/{id}")
+    public Caderno findById(@PathParam("id") Long id){
+        return repository.findById(id);
+    }
+
+    // Consulta por titulo
+    @GET
+    @Path("/search/titulo/{titulo}")
+    public List<Caderno> findByTitulo(@PathParam("titulo") String titulo){
+        return repository.findByTitulo(titulo);
+    }
+
+    // Procura o caderno pelo id e atualiza as informações
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Caderno update(@PathParam("id") Long id, Caderno cadernoAtualizado) {
+        Caderno cadernoExistente = repository.findById(id);
+        cadernoExistente.setTitulo(cadernoAtualizado.getTitulo());
+        cadernoExistente.setColecao(cadernoAtualizado.getColecao());
+        cadernoExistente.setTamanho(cadernoAtualizado.getTamanho());
+
+        repository.persist(cadernoExistente);
+
+        return cadernoExistente;
+    }
+
+    // Procura um caderno pelo id e deleta
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public void delete(@PathParam("id") Long id) {
+        Caderno cadernoParaExcluir = repository.findById(id);
+        
+        repository.delete(cadernoParaExcluir);
+                
     }
     
 }
