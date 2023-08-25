6dce97b72ba11fed598cb46439b544f098fb6d87
muniznicole <nicoleamuniz@gmail.com>
2023-08-25 20:59:10 -0300

    REMOVE ID

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,7 +5,7 @@
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
 
-insert into caderno (id, titulo, colecao, tamanho) values(1, 'Caderno Inteligente da Barbie', 'Mattel', 'P');
-insert into caderno (id, titulo, colecao, tamanho) values(2, 'Caderno Inteligente do BB-8', 'Star Wars', 'M');
-insert into caderno (id, titulo, colecao, tamanho) values(3, 'Caderno Inteligente do Mario', 'Super Mario Bros', 'G');
-insert into caderno (id, titulo, colecao, tamanho) values(4, 'Caderno Inteligente do Goku', 'Dragon Ball', 'P');
+insert into caderno (titulo, colecao, tamanho) values('Caderno Inteligente da Barbie', 'Mattel', 'P');
+insert into caderno (titulo, colecao, tamanho) values('Caderno Inteligente do BB-8', 'Star Wars', 'M');
+insert into caderno (titulo, colecao, tamanho) values('Caderno Inteligente do Mario', 'Super Mario Bros', 'G');
+insert into caderno (titulo, colecao, tamanho) values('Caderno Inteligente do Goku', 'Dragon Ball', 'P');
